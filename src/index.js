1bf4e56a235d4acea01fe56ed97cd3c0ed6353ae
aboynejames <james@aboynejames.co.uk>
2014-06-26 10:41:43 +0100

    readme documentation draft

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
 * @package    Self Engine open source project
 * @copyright  Copyright (c) 2013 James Littlejohn
 * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-* @version    v0.1.1
+* @version    v0.1.46
 */
 var server = require("./server");
 var router = require("./router");
